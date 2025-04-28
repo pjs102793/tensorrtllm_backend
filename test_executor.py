0246a0e3d25b2c676315461e3aa45af26da967e5
pjs102793 <pjs102793@naver.com>
2025-04-28 10:40:06 +0000

    feat: executro api ssfm sampling config 추가

--- test_executor.py
+++ test_executor.py
@@ -11,19 +11,46 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     kv_cache_config = trtllm.KvCacheConfig(enable_block_reuse=False)
+ 
+    # SamplingConfig(
+    #     end_id=8193,
+    #     pad_id=8807,
+    #     max_new_tokens=200, -> max_tokens
+    #     num_beams=1,
+    #     output_sequence_lengths=True,
+    #     temperature=0.8,
+    #     top_k=50,
+    #     top_p=0.8,
+    #     length_penalty=1.0,
+    #     early_stopping=1, -> use beam search
+    #     repetition_penalty=2.0,
+    #     min_length=1,
+    #     random_seed=None,
+    # )
+    sampling_kwargs = dict(
+        beam_width=1,
+        top_k=50,
+        top_p=0.8,
+        random_seed=42,
+        temperature=0.8,
+        repetition_penalty=2.0,
+        length_penalty=1.0,
+        num_return_sequences=1,
+        min_length=1,
+    )
+    # None 제거 (깔끔하게)
+    sampling_kwargs = {k: v for k, v in sampling_kwargs.items() if v is not None}
+
+    sampling_config = trtllm.SamplingConfig(**sampling_kwargs)
 
     # Create the executor.
     executor = trtllm.Executor(args.model_path, trtllm.ModelType.DECODER_ONLY, trtllm.ExecutorConfig(kv_cache_config=kv_cache_config))
 
     if executor.can_enqueue_requests():
         # Create the request.
-        request = trtllm.Request(input_token_ids=[1, 2, 3, 4, 5], max_tokens=30)
-        request2 = trtllm.Request(input_token_ids=[1, 2, 3], max_tokens=50)
-        request3 = trtllm.Request(input_token_ids=[1, 2, 3, 4], max_tokens=20)
-        request4 = trtllm.Request(input_token_ids=[1, 2, 3, 4, 5], max_tokens=40)
-        request5 = trtllm.Request(input_token_ids=[1, 2, 3, 4, 5], max_tokens=90)
-        
-        requests = [request, request2, request3, request4, request5]
+        request = trtllm.Request(input_token_ids=[1, 2, 8193], max_tokens=50, end_id=8193, pad_id=8807, sampling_config=sampling_config)
+
+        requests = [request, request, request, request, request]
 
         # Enqueue the request.
         request_ids = executor.enqueue_requests(requests)
@@ -39,7 +66,7 @@ if __name__ == "__main__":
                 print(f"[INFO] Output tokens: {resp.result.output_token_ids}")
                 all_responses.append(resp)
                 received += 1
-                
+
             # for stat in executor.get_latest_iteration_stats():
             #     value = stat.inflight_batching_stats.num_scheduled_requests
             #     print(value)
